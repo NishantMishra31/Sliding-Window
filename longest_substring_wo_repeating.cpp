a19d266dc276e06efeabc384e154c035b24a90fb
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-05 22:31:14 +0530

    Added Files and code changes

---
+++ longest_substring_wo_repeating.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    int n = s.size();
+    int len = 0, maxLen = 0;
+    for(int i = 0; i < n; i++){
+        unordered_map <int, int> mp;
+        for(int j = i; j < n; j++){
+            if(mp[s[j]] == 1) break;
+            len = j - i + 1;
+            maxLen = max(maxLen, len);
+            mp[s[j]] = 1;
+        }
+    }
+    return maxLen;
+}
+
+int main(){
+    string s;
+    cin >> s;
+    cout << lengthOfLongestSubstring(s);
+    return 0;
+}
---
+++ longest_substring_wo_repeating2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    int n = s.size();
+    vector <int> mp(256, -1);
+    int l = 0, r = 0, maxLen = 0;
+    while(r < n){
+        if(mp[s[r]] != -1){
+            if(mp[s[r]] >= l){
+                l = mp[s[r]] + 1;
+            }
+        }
+        int len = r - l + 1;
+        maxLen = max(maxLen, len);
+        mp[s[r]] = r;
+        r++;
+    }
+    return maxLen;
+}
+
+int main(){
+    string s;
+    cin >> s;
+    cout << lengthOfLongestSubstring(s);
+    return 0;
+}
