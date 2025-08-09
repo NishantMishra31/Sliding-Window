e06ce18fb50ec3380f2ef06e83c18b5e767b6bc2
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-09 23:24:46 +0530

    Added Files and code changes

---
+++ longest_repeating_char_replacement.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int characterReplacement(string s, int k) {
+    int n = s.size();
+    
+}
+
+int main(){
+    string s;
+    cin >> s;
+    int k;
+    cin >> k;
+    // call
+    cout << characterReplacement(s, k);
+    return 0;
+}
