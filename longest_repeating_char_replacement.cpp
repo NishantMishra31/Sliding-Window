88a94e40185cad30ff31cbb4b786b46d6aa9319f
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-09 23:37:19 +0530

    Added Files and code changes

--- longest_repeating_char_replacement.cpp
+++ longest_repeating_char_replacement.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/* You are given a string s and an integer k. 
+You can choose any character of the string and 
+change it to any other uppercase English character.
+You can perform this operation at most k times.
+
+Return the length of the longest substring containing
+the same letter you can get after performing the above operations. */
+
 int characterReplacement(string s, int k) {
     int n = s.size(), maxLen = 0;
     for(int i = 0; i < n; i++){
