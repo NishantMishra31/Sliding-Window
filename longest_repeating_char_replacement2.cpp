b091bddcf49f84deb0ce76f394533ea295b39066
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-09 23:56:11 +0530

    Added Files and code changes

--- longest_repeating_char_replacement2.cpp
+++ longest_repeating_char_replacement2.cpp
@@ -10,8 +10,24 @@ Return the length of the longest substring containing
 the same letter you can get after performing the above operations. */
 
 int characterReplacement(string s, int k) {
-    int n = s.size(), maxLen = 0;
-    
+    int n = s.size();
+    int l = 0, r = 0, maxLen = 0, maxFreq = 0;
+    vector <int> hash(26, 0);
+    while(r < n){
+        hash[s[r] - 'A']++;
+        maxFreq = max(maxFreq, hash[s[r] - 'A']);
+        while((r - l + 1 ) - maxFreq > k){
+            hash[s[l] - 'A']--;
+            maxFreq = 0;
+            for(int i = 0; i < 26; i++) maxFreq = max(maxFreq, hash[i]);
+            l++;
+        }
+        if((r - l + 1) - maxFreq <= k){
+            maxLen = max(maxLen, r - l + 1);
+            r++;
+        }
+    }
+    return maxLen;
 }
 
 int main(){
